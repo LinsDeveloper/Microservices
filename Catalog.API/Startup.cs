7fd63b29a8df005a93aeeea463612f54645c9c63
Pedro Lins <pedrolins42@gmail.com>
2023-09-18 04:03:39 -0300

    Finalização da Integração com o Datadog.

--- Catalog.API/Startup.cs
+++ Catalog.API/Startup.cs
@@ -1,10 +1,5 @@
 ﻿using Catalog.API.Data;
 using Catalog.API.Repositories;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
 namespace Catalog.API
